ed133fbcfbaa902f8ac50bcea4e5d5583291e60e
Tapas <tapasmahanandia@gmail.com>
2021-01-16 11:37:43 +0530

    node js files updated

---
+++ cs50 Mobile/part1/syntax.js
@@ -0,0 +1,23 @@
+// node command prefix with double share
+/* multi line looks
+*/
+// basic syntas to declare node js script
+const firstName = "Tapas";
+
+const lastName  = "Mahanandia";
+
+//printing out log 
+//console.log(firstName);
+//console.log(lastName);
+
+// array function delared in line 
+const arr = [
+  'String', 42,
+  function() {console.log('hi')}
+]
+//arr[2]()
+for (let i = 0; i < arr.length; i++)
+{
+  console.log(arr[i])
+}
+
---
+++ cs50 Mobile/part1/type.js
@@ -0,0 +1,7 @@
+const x = 42
+
+// get type by using "typeof"
+console.log(typeof x)
+console.log(typeof undefined)
+//This may surpise you 
+console.log(typeof null)
